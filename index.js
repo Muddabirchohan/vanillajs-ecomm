cb18638e51b0fab4b1fc7cea96f0ade779ef7d21
Muddabir Chohan <muddabir22@gmail.com>
2023-12-24 22:51:26 +0500

    loader added to list page

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-window.onload = function() {
+
+
+window.onload = function () {
+
+
 
     fetch("https://rickandmortyapi.com/api/character")
         .then(res => res.json())
         .then(res => {
             setTimeout(() => {
                 document.getElementById("loader").innerHTML = "";
-             
+
                 document.getElementsByClassName("loader-parent")[0].classList.remove("loader-parent");
                 document.getElementById("loader").removeAttribute("id")
 
@@ -34,8 +38,40 @@ const nodeCreate = (nodeList, result) => {
             node.innerHTML = result.name;
         }
 
+        containerDiv.onclick = function () {
+
+            // Example usage:
+            const originalUrl = window.location.href;
+            const updatedLastPart = "detail.html";
+            const stateData = result.id;
+
+            const updatedUrl = updateLastPartOfUrl(originalUrl, updatedLastPart, stateData);
+
+            window.location.href = updatedUrl.href + updatedUrl.newUrl;
+        };
         containerDiv.appendChild(node);
     });
 
     list.appendChild(containerDiv);
 };
+
+
+function updateLastPartOfUrl(baseUrl, updatedLastPart, state) {
+    const url = new URL(baseUrl);
+    const pathParts = url.pathname.split('/');
+    pathParts[pathParts.length - 1] = updatedLastPart;
+    url.pathname = pathParts.join('/');
+
+    // history.pushState({ state }, "", url.href);
+    // Get the current URL parameters
+    const urlParams = new URLSearchParams(window.location.search);
+
+    // Set a parameter in the URL
+    urlParams.set('id', state);
+
+    // Update the URL with the new parameters
+    const newUrl = `?${urlParams.toString()}`;
+
+    return { href: url.href, newUrl };
+}
+
